90e83b580abd803bf5af970ec7425839f583f34b
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-03 23:45:27 +0600

    add blog page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
 
 const auth = getAuth(app);
 
 const Register = () => {
+    const googleProvider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        console.log('google mama');
+    }
+
     const [error, setError] = useState('');
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const form = event.target;
@@ -66,7 +73,7 @@ const Register = () => {
                     <p className='text-center fw-semibold mt-4'> Or Sign up with</p>
                 </div>
                 <div className='d-flex justify-content-center align-items-center gap-4'>
-                    <Button variant="outline-primary"> <img src="https://cdn-icons-png.flaticon.com/512/300/300221.png?w=740&t=st=1683030098~exp=1683030698~hmac=a172fbf96bc191335562a518ae58b3fe0d5807cd0aa88a9835adf20c6d03047b" alt="" style={{ height: "18px" }} className='' /> Google</Button>
+                    <Button onClick={handleGoogleSignIn} variant="outline-primary"> <img src="https://cdn-icons-png.flaticon.com/512/300/300221.png?w=740&t=st=1683030098~exp=1683030698~hmac=a172fbf96bc191335562a518ae58b3fe0d5807cd0aa88a9835adf20c6d03047b" alt="" style={{ height: "18px" }} className='' /> Google</Button>
                     <Button variant="outline-primary"> <img src="https://cdn-icons-png.flaticon.com/512/733/733609.png?w=740&t=st=1683032868~exp=1683033468~hmac=f8415bbd3b9f44c83a6fc67837cddfa6e2ffcae53ccc7cc709f64b9664d7b433" alt="" style={{ height: "18px" }} /> Github</Button>
                 </div>
                 <div className='text-center mt-5'>
